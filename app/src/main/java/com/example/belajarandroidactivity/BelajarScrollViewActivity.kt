239111ccb4b63e9eb394afb4d818db6ef78d349c
TEHFAAAA <fateehganteng@gmail.com>
2023-09-07 10:12:51 +0700

    7 September 2023

--- app/src/main/java/com/example/belajarandroidactivity/BelajarScrollViewActivity.kt
+++ app/src/main/java/com/example/belajarandroidactivity/BelajarScrollViewActivity.kt
@@ -2,10 +2,19 @@ package com.example.belajarandroidactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import jp.wasabeef.glide.transformations.BlurTransformation
 
 class BelajarScrollViewActivity : AppCompatActivity() {
+    var ivBackground: ImageView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_belajar_scroll_view)
+        ivBackground = findViewById(R.id.ivBg)
+        Glide.with(this)
+            .load(R.drawable.german)
+            .transform(BlurTransformation(25, 3))
+            .into(ivBackground!!)
     }
 }
