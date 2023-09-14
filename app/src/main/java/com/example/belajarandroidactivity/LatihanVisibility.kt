742b5b82c39d392dadc7a913c588c8681242f920
TEHFAAAA <fateehganteng@gmail.com>
2023-09-14 11:18:50 +0700

    14 September 2023

---
+++ app/src/main/java/com/example/belajarandroidactivity/LatihanVisibility.kt
@@ -0,0 +1,11 @@
+package com.example.belajarandroidactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LatihanVisibility : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_latihan_visibility)
+    }
+}
