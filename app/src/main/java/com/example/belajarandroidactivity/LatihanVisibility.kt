9b52697be5ef0ce0e1fbd4fed1560aeab3e85d87
TEHFAAAA <fateehganteng@gmail.com>
2023-09-14 13:22:57 +0700

    14 September 2023

--- app/src/main/java/com/example/belajarandroidactivity/LatihanVisibility.kt
+++ app/src/main/java/com/example/belajarandroidactivity/LatihanVisibility.kt
@@ -2,10 +2,24 @@ package com.example.belajarandroidactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class LatihanVisibility : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_latihan_visibility)
+        val tvText:TextView = findViewById(R.id.tvText)
+        val btnShow: Button = findViewById(R.id.btnShow)
+
+        btnShow.setOnClickListener {
+            if (tvText.visibility == View.VISIBLE) {
+                tvText.visibility = View.INVISIBLE
+            }
+            else {
+                tvText.visibility = View.VISIBLE
+            }
+        }
     }
 }
